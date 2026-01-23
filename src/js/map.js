6b38ed1a74e50bac6816cc8f62b9dc7b2dff5ee4
Denis Sergeev <denis-sergeev-85@inbox.ru>
2026-01-23 16:40:26 +0300

    исправлена метка карты

--- src/js/map.js
+++ src/js/map.js
@@ -6,6 +6,19 @@ function init() {
     zoom: 17,
   });
 
+  // SVG иконка
+  const svgIcon = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 16 16" fill="none">
+      <path d="M8 1C4.6862 1 2 3.6862 2 7C2 8.6563 2.6711 10.156 3.7565 11.2417C4.8422 12.328 7.4 13.9 7.55 15.55C7.57249 15.7974 7.7516 16 8 16C8.2484 16 8.42751 15.7974 8.45 15.55C8.6 13.9 11.1578 12.328 12.2435 11.2417C13.3289 10.156 14 8.6563 14 7C14 3.6862 11.3138 1 8 1Z" fill="#FF4433"/>
+      <path d="M8.00002 9.10015C9.15982 9.10015 10.1 8.15994 10.1 7.00015C10.1 5.84035 9.15982 4.90015 8.00002 4.90015C6.84023 4.90015 5.90002 5.84035 5.90002 7.00015C5.90002 8.15994 6.84023 9.10015 8.00002 9.10015Z" fill="white"/>
+    </svg>
+  `;
+
+  // Создаем макет для иконки
+  const iconLayout = ymaps.templateLayoutFactory.createClass(
+    `<div style="width: 40px; height: 40px;">${svgIcon}</div>`,
+  );
+
   let balloonContent = `
     <div style="width: 140px; padding: 5px; border-radius: 5px !important; background: white;">
       <p>ул. Гаранина 15к3</p>
@@ -18,11 +31,14 @@ function init() {
       balloonContent: balloonContent,
     },
     {
-      preset: "islands#redStretchyIcon",
-      iconImageSize: [80, 80],
-      iconImageOffset: [0, 0],
+      iconLayout: iconLayout, // Используем нашу кастомную иконку
+      iconShape: {
+        type: "Circle",
+        coordinates: [0, 0],
+        radius: 20,
+      },
       balloonAutoPan: false,
-      balloonOffset: [-50, -70],
+      balloonOffset: [-50, -40],
       balloonCloseButton: false,
       hideIconOnBalloonOpen: false,
       balloonPanelMaxMapArea: 0,
