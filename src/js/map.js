2d3bfd504b4336e73c1e5b8b89366e2bb97a0c25
Denis Sergeev <denis-sergeev-85@inbox.ru>
2026-01-23 15:05:52 +0300

    заменён лого

--- src/js/map.js
+++ src/js/map.js
@@ -1,22 +1,55 @@
+let center = [55.027423, 82.96716];
+
 function init() {
   let map = new ymaps.Map("map", {
-    center: [55.027423, 82.96716],
+    center: center,
     zoom: 17,
   });
 
-  var placemark = new ymaps.Placemark(
-    [55.027423, 82.96716],
+  let balloonContent = `
+    <div style="width: 140px; padding: 5px; border-radius: 5px !important; background: white;">
+      <p>ул. Гаранина 15к3</p>
+    </div>
+  `;
+
+  let placemark = new ymaps.Placemark(
+    center,
     {
-      balloonContent: "ул Гаранина 15 к3",
+      balloonContent: balloonContent,
     },
     {
-      preset: "islands#redStretchyIcon", // Другая метка
-      iconColor: "#ff0000", // Ярко-красный цвет
+      preset: "islands#redStretchyIcon",
+      iconImageSize: [80, 80],
+      iconImageOffset: [0, 0],
+      balloonAutoPan: false,
+      balloonOffset: [-50, -70],
+      balloonCloseButton: false,
+      hideIconOnBalloonOpen: false,
+      balloonPanelMaxMapArea: 0,
+      balloonShadow: false,
+      balloonLayout: ymaps.templateLayoutFactory.createClass(balloonContent),
     },
   );
 
+  map.controls.remove("geolocationControl");
+  map.controls.remove("searchControl");
+  map.controls.remove("trafficControl");
+  map.controls.remove("typeSelector");
+  map.controls.remove("fullscreenControl");
+  map.controls.remove("zoomControl");
+  map.controls.remove("rulerControl");
+
   map.geoObjects.add(placemark);
-  placemark.balloon.open();
+
+  setTimeout(function () {
+    placemark.balloon.open();
+
+    const balloon = document.querySelector(".ymaps-balloon");
+    if (balloon) {
+      balloon.style.borderRadius = "12px";
+      balloon.style.overflow = "hidden";
+    }
+  }, 300);
 }
 
 ymaps.ready(init);
